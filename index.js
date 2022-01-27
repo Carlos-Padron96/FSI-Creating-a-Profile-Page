92b90bf2a49b2f2a0f7baeacda11e993eaf5b19b
Carlos-Padron96 <cpadron96@yahoo.com>
2022-01-26 20:45:06 -0800

    image added way too big

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 let content = document.querySelector('.js-generated.content')
+let header = document.createElement('h1')
+header.setAttribute('class', 'dog-name')
+header.append('Rizzo')   
+content.append(header) 
+let img = document.createElement('img')
+img.src = "assets/rizzo.jpg"
+img.append('img')
+content.append(img)
